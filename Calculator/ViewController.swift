6e054480c12d6a16fce2bdf39f727f9c81d92b23
a.matsu <albel.gm@gmail.com>
2015-06-29 19:20:11 +0900

    Show Errro/ Bugfix/ Code format

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //----- enum -----//
     // 計算機状態定義
     enum CalcState {
         case DuringInput1
@@ -17,12 +18,34 @@ class ViewController: UIViewController {
         case DuringInput2
         case Calculated
     }
+    
+    // エラー種類。エラー時に表示する文字列も一緒に定義。
+    enum ErrorType {
+        case ZeroDivide
+        case Overflow
+        func toString() -> String {
+            switch self {
+            case .ZeroDivide:
+                return "エラー:0除算"
+            case .Overflow:
+                return "エラー:オーバーフロー"
+            }
+        }
+    }
 
+    //----- field -----//
     // 初期状態は第1数入力中
     var state = CalcState.DuringInput1
 
-    // 数字キー
+    // 入力(文字列)
+    var val1str: String = ""
+    var val2str: String = ""
+    var operatorType: String = ""
+
+    // 入出力/結果 表示Label
     @IBOutlet weak var display: UILabel!
+    
+    // 数字キー
     @IBAction func buttonDigit(sender: UIButton) {
         switch state {
         case .DuringInput1:
@@ -30,7 +53,6 @@ class ViewController: UIViewController {
                 display.text = sender.currentTitle!
             } else {
                 display.text = addDigitString(display.text!, add:sender.currentTitle!)
-//                display.text = display.text! + sender.currentTitle!
             }
             println("\(CalcState.DuringInput1) disp:\(display.text) val1:\(val1str) val2:\(val2str)")
             break
@@ -42,7 +64,6 @@ class ViewController: UIViewController {
             break
         case .DuringInput2:
             display.text = addDigitString(display.text!, add:sender.currentTitle!)
-//            display.text = display.text! + sender.currentTitle!
             println("\(CalcState.DuringInput2) disp:\(display.text) val1:\(val1str) val2:\(val2str)")
             break
         case .Calculated:
@@ -72,7 +93,6 @@ class ViewController: UIViewController {
     }
     
     // + - * / オペレーターボタン
-    var operatorType: String = ""
     @IBAction func buttonOperator(sender: UIButton) {
         switch state {
         case .DuringInput2:
@@ -124,59 +144,87 @@ class ViewController: UIViewController {
             display.text = display.text! + "."
         }
     }
-    
-    var val1str: String = ""
-    var val2str: String = ""
-    
-    func str2value() -> Double {
-        return atof(display.text!)
+
+    // display Labelにエラー文字列をセットして表示
+    func showError(errorType: ErrorType) {
+        display.text = errorType.toString()
     }
 
-
-    func showError() {
-        display.text = "エラー"
-    }
-    
+    // これまでの入力値から計算 -> 値の保持とセットで別クラスに切り出したい。余裕あればやる。
     func Calculate() {
         // doubleの有効桁数は15桁程度
         // -> http://www.cc.kyoto-su.ac.jp/~yamada/programming/float.html#double
-        var first: Double = atof(val1str)
-        var second: Double = atof(val2str)
+        let first: Double = atof(val1str)
+        let second: Double = atof(val2str)
+        var result: Double = 0.0
 
         switch operatorType {
         case "+":
-            display.text = String("\(first + second)")
+            result = first + second
             break
         case "-":
-            display.text = String("\(first - second)")
+            result = first - second
             break
         case "×":
-            display.text = String("\(first * second)")
+            result = first * second
             break
         case "÷":
             if second == 0 {
-                showError()
+                showError(ErrorType.ZeroDivide)
             } else {
-                display.text = String("\(first / second)")
+                result = first / second
             }
             break
         default:
             break
         }
 
-        display.text = cutoff(display.text!)
+        if result >= DBL_MAX {
+            showError(ErrorType.Overflow)
+        } else {
+            display.text = hoge(result)
+        }
 
-        // 現在の表示を第1オペランド、
+        // 現在の表示を第1オペランドに
         val1str = display.text!
-        println("calc: val1str:\(val1str) display:\(display.text!)")
     }
 
-    func cutoff(text: String) -> String {
-        let r = text.rangeOfString(".");
-        let idx = advance(r!.startIndex, 1)
-        if atoi(text.substringFromIndex(idx)) == 0 {
-            let integerIdx = advance(r!.startIndex, 0)
-            return text.substringToIndex(integerIdx)
+    func hoge(var value:Double) -> String {
+        if value >= 1.0e+10 {
+            var i: Int
+            for i = 0; value >= 10.0; i++ {
+                value /= 10
+            }
+            let str = cutoff(String("\(value)"), needRound:true) + String("e+\(i)")
+            println(str)
+            return str
+        } else if value <= 1.0e-8 {
+            var i: Int
+            for i = 0; value < 1.0; i++ {
+                value *= 10
+            }
+            let str = cutoff(String("\(value)"), needRound:true) + String("e-\(i)")
+            println("hoge: value:\(value)")
+            println("hoge: \(str)")
+            return str
+        }
+        return cutoff(String("\(value)"), needRound:false)
+    }
+
+    func cutoff(var text: String, needRound: Bool) -> String {
+        if let r = text.rangeOfString(".") {
+            let idx = advance(r.startIndex, 1)
+            if atoi(text.substringFromIndex(idx)) == 0 {
+                // 小数点以下が値を持たない場合、整数部だけの文字列に
+                let integerIdx = advance(r.startIndex, 0)
+                return text.substringToIndex(integerIdx)
+            } else if needRound {
+                // 10の累乗表示する場合、小数点以下6桁で値を丸める
+                let ORDER_OF_DISP = 1000000.0
+                var num = round(atof(text) * ORDER_OF_DISP) / ORDER_OF_DISP
+                println("num:\(num)")
+                return String("\(num)")
+            }
         }
         return text
     }
